d903cef78153d05379df16d01ce573c204c3158f
root <root@Kabod>
2024-04-05 15:34:23 +0200

    dbase

--- src/views/Chatbot/bot-message.js
+++ src/views/Chatbot/bot-message.js
@@ -1,10 +1,17 @@
-export default (botName, message, botImage) => (`
-<div class="card-body shadow-lg p-3 mb-5 mx-5 bg-body-tertiary rounded mt-5">
-  <div class="bot-picture">
-    <img src="${botImage}" width="90px">
-  </div>
-  <h5 class="card-title">${botName}</h5>
-  <p class="card-text container-fluid">${message}</p>
-  <span>${Date()}</span>
-</div>
-`);
+export default (botName, message, botImage) => {
+  const botmessage = {
+    image: botImage,
+    name: botName,
+    actions: message
+  };
+  return (`
+    <div class="card-body shadow-lg p-3 mb-5 mx-5 bg-body-tertiary rounded mt-5">
+      <div class="bot-picture">
+        <img src="${botmessage.image}" width="90px">
+      </div>
+      <h5 class="card-title">${botmessage.name}</h5>
+      <p class="card-text container-fluid"> ${botmessage.actions}</p>
+      <span>${Date()}</span>
+    </div>
+  `);
+};
--- src/views/Chatbot/user-message.js
+++ src/views/Chatbot/user-message.js
@@ -1,11 +1,21 @@
-export default (username, messageText) => (`
+export default (messageText) => {
+  const usermessage = {
+    author: 'keanu Richard',
+    avatar: 'https://freesvg.org/img/1538298822.png',
+    type: 'user',
+    date: Date(),
+    text: messageText
+  };
+  return (`
   <div class="row">
     <div class="col-6 card-body shadow-lg p-3 mb-5 mx-5 bg-body-tertiary rounded mt-3">
       <div class="user-picture">
-        <img src="https://freesvg.org/img/1538298822.png" width="90px">
+        <img src="${usermessage.avatar}" width="90px">
       </div>
-      <h5 class="username">${username}</h5>
-      <p class="user-text container-fluid">${messageText}.</p>
+      <h5 class="username">${usermessage.author}</h5>
+      <p class="user-text container-fluid">${usermessage.text}.</p>
+      <span>${usermessage.date}</span>
     </div>
   </div>
-`);
+  `);
+};
